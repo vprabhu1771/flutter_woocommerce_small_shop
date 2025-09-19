e91c24e7e449588d1fb40321bc234f1c583e36cd
Prabhu <vprabhu1771@gmail.com>
2025-09-19 19:53:06 +0530

    env connection added to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+
+  // Load .env before running the app
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
